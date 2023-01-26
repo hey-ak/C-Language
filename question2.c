fe40848eb9a03ee5280270362889600e33592fe4
Akshay <akshay0127.be21@chitkara.edu.in>
2023-01-26 19:57:24 +0530

    Added new questions in c

---
+++ question2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    // area of square
+    // float side;
+    // printf("Enter The Side Of Square");
+    // scanf("%f",&side);
+    // float area= side*side;
+    // printf("The area of square is %f", area);
+
+    // area of circle
+    float circleRadius;
+    printf("Enter Radius");
+    scanf("%f",&circleRadius);
+    float area = 3.14*(circleRadius*circleRadius);
+    printf("The area is %f", area);
+
+    
+}
---
+++ questions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("Enter first Number");
+    scanf("%d", &a);
+
+    printf("Enter second Number");
+    scanf("%d", &b);
+
+    int sum = a + b;
+    printf("The Sum is : %d ", sum);
+
+
+
+    return 0;
+}
