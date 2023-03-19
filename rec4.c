a268b55a78967dbc1c0fd2d2fde110ff27fd5c1a
Akshay <akshay0127.be21@chitkara.edu.in>
2023-03-19 16:04:02 +0530

    added palindrome string program added

--- rec4.c
+++ rec4.c
@@ -1,28 +1,56 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-void swapArray(int arr[], int start, int end) {
+// void swapArray(int arr[], int start, int end) {
+//    if (start>=end)
+//    {
+//     return;
+//    }
+//    int temp= arr[start];
+//    arr[start]= arr[end];
+//    arr[end]= temp;
+//    swapArray(arr,start+1,end-1);
+
+   
+// }
+
+// int main() {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     printf("Original array: ");
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+//     swapArray(arr, 0, n-1);
+//     printf("Swapped array: ");
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+// problem -2 palindrome string check
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char str[], int start, int end) {
     if (start >= end) {
-        return;
+        return 1;
     }
-    int temp = arr[start];
-    arr[start] = arr[end];
-    arr[end] = temp;
-    swapArray(arr, start+1, end-1);
+    if (str[start] != str[end]) {
+        return 0;
+    }
+    return isPalindrome(str, start+1, end-1);
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Original array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    char str[] = "racecar";
+    int n = strlen(str);
+    int result = isPalindrome(str, 0, n-1);
+    if (result == 1) {
+        printf("%s is a palindrome\n", str);
+    } else {
+        printf("%s is not a palindrome\n", str);
     }
-    printf("\n");
-    swapArray(arr, 0, n-1);
-    printf("Swapped array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
     return 0;
 }
